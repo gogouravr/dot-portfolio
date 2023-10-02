f5fee7986f5f526432a6aba1bdb398eb678e2fd6
gouravrajbit <gouravr244@gmail.com>
2023-10-02 19:26:02 +0530

    Add `résumé` button

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import SocialMediaSection from './SocialMediaSection.jsx';
 import config from '../config.json'
+import PrimaryButton from './PrimaryButton.jsx';
 
 export default function Intro() {
+    const resumeButtonClickHandler = () => {
+        window.open('/Gourav_Raj_Full_Stack_Developer.pdf', '_blank');
+    }
     return (
         <section className="flex flex-col justify-center">
             <div className="text-white font-normal text-2xl mt-20 mb-2 opacity-80">
@@ -16,7 +20,10 @@ export default function Intro() {
             <div className="text-white font-high-l  text-5xl mt-5 text-2 opacity-50 leading-tight">
                 And I am a Full-Stack Developer
             </div>
+
             <SocialMediaSection />
+
+            <PrimaryButton text="Résumé" clickHandler={resumeButtonClickHandler}>Résumé</PrimaryButton>
         </section>
     )
 }
---
+++ src/components/PrimaryButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function PrimaryButton(props) {
+    return (
+        <div className='text-white font-semibold mt-12 text-opacity-80 flex' onClick={props.clickHandler}>
+            <button className='border-2 border-sky-500 px-4 py-3 rounded-lg cursor-pointer  hover:border-t-4  hover:border-l-4 ease-in duration-300'>
+                {props.children}
+            </button>
+        </div>
+    )
+}
