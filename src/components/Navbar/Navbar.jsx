c129449bd2b8f2b31ac45c052f3f7a889e3074ba
gouravrajbit <gouravr244@gmail.com>
2023-10-05 00:06:51 +0530

    Navbar styling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
+import PrimaryButton from '../UI/PrimaryButton'
 
 export default function Navbar() {
     return (
-        <nav className='flex justify-around'>
-            <section className='text-white'>
+        <nav className='flex justify-between container mx-auto mt-3 items-center'>
+            <section className='text-white text-3xl font-prime font-semibold opacity-90 tracking-wide'>
                 Portfolio.
             </section>
-            <section className='text-white flex'>
-                <li className='list-none'>About</li>
-                <li className='list-none'>Experience</li>
-                <li className='list-none'>Projects</li>
-                <li className='list-none'>Contact</li>
+            <section className='text-white flex gap-10 items-center font-high-l'>
+                <li className='list-none cursor-pointer hover:text-sky-500 ease-in-out duration-100 opacity-85'>About</li>
+                <li className='list-none cursor-pointer hover:text-sky-500 ease-in-out duration-100 opacity-85'>Experience</li>
+                <li className='list-none cursor-pointer hover:text-sky-500 ease-in-out duration-100 opacity-85'>Projects</li>
+                <li className='list-none cursor-pointer hover:text-sky-500 ease-in-out duration-100 opacity-85'>Contact</li>
+                <li className='list-none cursor-pointer hover:text-sky-500 ease-in-out duration-100 opacity-85'><PrimaryButton>Say Hello</PrimaryButton></li>
             </section>
         </nav>
     )
