13799815c38c3a46b3b9ea088a5fa68a82c27b7e
gouravrajbit <gouravr244@gmail.com>
2023-10-05 22:24:42 +0530

    Shadow effect to the button

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,17 +3,17 @@ import PrimaryButton from '../UI/PrimaryButton'
 
 export default function Navbar() {
     return (
-        <nav className='flex justify-between container mx-auto mt-3 items-center'>
+        <nav className='flex justify-between container mx-auto mt-4 items-center'>
             <section className='text-white text-3xl font-prime font-semibold opacity-90 tracking-wide'>
                 Portfolio<span className="text-sky-500">.</span>
             </section>
-            <section className='text-white flex gap-10 items-center font-high-l'>
+            <ul className='text-white flex gap-10 items-center font-high-l'>
                 <li className='list-none cursor-pointer hover:text-sky-500 ease-in-out duration-100 opacity-85'>About</li>
                 <li className='list-none cursor-pointer hover:text-sky-500 ease-in-out duration-100 opacity-85'>Experience</li>
                 <li className='list-none cursor-pointer hover:text-sky-500 ease-in-out duration-100 opacity-85'>Projects</li>
                 <li className='list-none cursor-pointer hover:text-sky-500 ease-in-out duration-100 opacity-85'>Contact</li>
                 <li className='list-none cursor-pointer hover:text-sky-500 ease-in-out duration-100 opacity-85'><PrimaryButton>Say Hello</PrimaryButton></li>
-            </section>
+            </ul>
         </nav>
     )
 }
--- src/components/UI/PrimaryButton.jsx
+++ src/components/UI/PrimaryButton.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function PrimaryButton(props) {
     return (
-        <div className='text-white font-semibold text-opacity-80 flex' onClick={props.clickHandler}>
-            <button className='border-2 border-sky-500 px-4 py-2 rounded-lg cursor-pointer  hover:border-t-4  hover:border-l-4 ease-in duration-300'>
+        <div className='text-white font-semibold text-opacity-80 flex box-border' onClick={props.clickHandler}>
+            <button className='border border-sky-500 px-4 py-2 rounded cursor-pointer hover:translate-x-[-3px] hover:translate-y-[-3px] hover:shadow-sky-500  hover:shadow-[4px_4px_0px_0px] ease-in duration-300'>
                 {props.children}
             </button>
         </div>
