695267cf7858d8e510dbe4dc4acfdbe5d53cd499
gouravrajbit <gouravr244@gmail.com>
2023-10-07 01:57:03 +0530

    Make navbar responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,12 +8,12 @@ export default function Navbar() {
     }
 
     return (
-        <nav className='flex justify-between container mx-auto mt-4 items-center'>
+        <nav className='flex container w-5/6 mx-auto mt-5 items-center justify-center md:justify-between'>
             <section className='text-white text-3xl font-prime font-semibold opacity-90 tracking-wide'>
                 gouravraj<span className="text-sky-500">.</span>in
             </section>
-            <ul className='text-white flex gap-10 items-center font-normal'>
-                <li className='list-none font-thin cursor-pointer hover:text-sky-400 ease-in-out duration-100 opacity-85'>About</li>
+            <ul className='text-white items-center font-normal hidden md:gap-4 lg:gap-8 xl:gap-10 md:flex'>
+                <li className='list-none font-thin cursor-pointer hover:text-sky-400 ease-in-out duration-100 opacity-85 ml-10 lg:ml-0'>About</li>
                 <li className='list-none font-thin cursor-pointer hover:text-sky-400 ease-in-out duration-100 opacity-85'>Experience</li>
                 <li className='list-none font-thin cursor-pointer hover:text-sky-400 ease-in-out duration-100 opacity-85'>Projects</li>
                 <li className='list-none font-thin cursor-pointer hover:text-sky-400 ease-in-out duration-100 opacity-85'>Contact</li>
