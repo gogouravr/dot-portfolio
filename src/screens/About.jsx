151435be293532a4cefb400c5f5db92fb2629028
gouravrajbit <gouravr244@gmail.com>
2023-10-13 01:39:02 +0530

    Update style for About screen

--- src/screens/About.jsx
+++ src/screens/About.jsx
@@ -7,13 +7,13 @@ import { faAddressCard } from '@fortawesome/free-solid-svg-icons';
 export default function About() {
     return (
         <div className='my-20 a sm:mt-10 md:mt-40 lg:mt-60 mx-auto w-11/12 sm:w-10/12 md:w-9/12 lg:w-8/12' id='about'>
-            <h2 className='text-white font-medium text-2xl font-high-l self-start lg:text-3xl relative hor-bar first:mr-2 opacity-70'>
+            <h2 className='text-white font-medium text-2xl font-high-l text-center lg:text-start lg:self-start lg:ml-[33.3%] lg:text-3xl relative hor-bar opacity-70'>
                 <FontAwesomeIcon icon={faAddressCard} className='mr-3' />
                 About Me
             </h2>
-            <section className='flex flex-col lg:flex-row md:gap-8 lg:gap-12 justify-center items-center lg:items-start mt-8 a sm:mt-10 mx-auto w-11/12 sm:w-10/12 md:w-9/12 lg:w-11/12'>
+            <section className='flex flex-col lg:flex-row mx-auto lg:justify-between items-center lg:items-start mt-8 sm:mt-6 w-11/12 sm:w-10/12 md:w-9/12 lg:w-fit'>
 
-                <figure className='mt-10 w-3/4 max-w-[300px] lg:w-1/3 lg:max-w-none image-container relative'>
+                <figure className='mt-10 w-3/4 max-w-[300px] lg:w-[25%] lg:max-w-none image-container relative rounded-full'>
                     <img className='border-3 shadow-[0_0_20px_20px_rgba(14,165,233,0.80)] border-sky-500 opacity-80 rounded-full drop-shadow-2xl bg-slate-800' src="/gourav-raj-photo.png" alt={profile['first-name'] + ' ' + profile['last-name']} />
                 </figure>
 
