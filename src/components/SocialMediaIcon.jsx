6123bdc4670166dc62063c37ace6d074aa6ee9d3
gouravrajbit <gouravr244@gmail.com>
2023-10-17 00:04:35 +0530

    Add light theme to social media icons

--- src/components/SocialMediaIcon.jsx
+++ src/components/SocialMediaIcon.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub, faLinkedin, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faGithub, faLinkedin, faInstagram, faTwitter, faGith } from '@fortawesome/free-brands-svg-icons';
 import profile from '../profile.json';
+
 
 export default function SocialMediaIcon({ type, iconSize }) {
     const iconTypes = {
@@ -15,7 +16,7 @@ export default function SocialMediaIcon({ type, iconSize }) {
     const URL = profile['social-media-links'][type];
 
     return (
-        <a href={URL} target='_blank' rel='noreferrer' className='cursor-pointer opacity-80 focus-visible:outline-dashed focus-visible:outline-2 focus-visible:outline-sky-400 rounded px-1 py-1'>
+        <a href={URL} target='_blank' rel='noreferrer' className='text-slate-900 dark:text-white cursor-pointer dark:opacity-80 focus-visible:outline-dashed focus-visible:outline-2 focus-visible:outline-sky-400 rounded px-1 py-1'>
             <FontAwesomeIcon icon={icon} fontSize={iconSize} className='transition ease-in-out duration-230 hover:-translate-y-2' />
         </a>
     )
