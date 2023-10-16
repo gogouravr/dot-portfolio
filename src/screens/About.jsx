d1e4920b9fadf201e62c0bebc533bfa41a02cc24
gouravrajbit <gouravr244@gmail.com>
2023-10-17 00:37:47 +0530

    Add light theme to Experience screen

--- src/screens/About.jsx
+++ src/screens/About.jsx
@@ -7,7 +7,7 @@ import { faAddressCard } from '@fortawesome/free-solid-svg-icons';
 export default function About() {
     return (
         <div className='dark:text-white text-slate-900 my-20 a sm:mt-10 md:mt-40 lg:mt-60 mx-auto w-11/12 sm:w-10/12 md:w-9/12 lg:w-8/12' id='about'>
-            <h2 className='dark:text-white text-sky-700 font-medium text-2xl font-high-l text-center lg:text-start lg:self-start lg:ml-[33.3%] lg:text-3xl opacity-70'>
+            <h2 className='dark:text-white text-sky-900 font-medium text-2xl font-high-l text-center lg:text-start lg:self-start lg:ml-[33.3%] lg:text-3xl dark:opacity-80'>
                 <FontAwesomeIcon icon={faAddressCard} className='mr-3' />
                 <span className='relative hor-bar'>
                     About Me
@@ -16,10 +16,10 @@ export default function About() {
             <section className='flex flex-col lg:flex-row mx-auto lg:justify-between items-center lg:items-start mt-8 sm:mt-5 w-fit'>
 
                 <figure className='mt-10 w-3/4 max-w-[300px] lg:w-[25%] lg:max-w-none image-container relative rounded-full'>
-                    <img className='border-3 shadow-[0_0_20px_20px_rgba(14,165,233,0.80)] border-sky-500 opacity-80 rounded-full drop-shadow-2xl dark:bg-slate-800 bg-sky-200' src="/gourav-raj-photo.png" alt={profile['first-name'] + ' ' + profile['last-name']} />
+                    <img className='border-3 shadow-[0_0_20px_20px_rgba(14,165,233,0.80)] border-sky-500 dark:opacity-80 opacity-95 rounded-full drop-shadow-2xl dark:bg-slate-800 bg-sky-200' src="/gourav-raj-photo.png" alt={profile['first-name'] + ' ' + profile['last-name']} />
                 </figure>
 
-                <p className='font-high-l dark:font-[280] font-[400] leading-6 lg:w-2/3 opacity-80 mt-8 lg:mt-5 text-base lg:text-[1.1rem] tracking-normal flex flex-col gap-3'>
+                <p className='font-high-l dark:font-[280] font-[400] leading-6 lg:w-2/3 dark:opacity-80 mt-8 lg:mt-5 text-base lg:text-[1.1rem] tracking-normal flex flex-col gap-3'>
                     <p>{profile['about-text'][0]}</p>
                     <p>{profile['about-text'][1]}
                         <a href='https://www.bitmesra.ac.in/' target='_blank' rel="noreferrer" className="text-sky-500 dark:font-light font-[400]">
--- src/screens/Experience.jsx
+++ src/screens/Experience.jsx
@@ -5,8 +5,8 @@ import { faBriefcase } from '@fortawesome/free-solid-svg-icons';
 
 export default function Experience() {
     return (
-        <div className='flex flex-col items-center md:items-end  my-20 sm:mt-10 md:mt-40 lg:mt-60 mx-auto w-11/12 sm:w-10/12 md:w-9/12 lg:w-8/12' id='experience'>
-            <h2 className='text-white font-medium text-2xl font-high-l lg:ml-[33.3%] mx-auto md:self-start lg:text-3xl opacity-70'>
+        <div className='dark:text-white text-slate-900 flex flex-col items-center md:items-end  my-20 sm:mt-10 md:mt-40 lg:mt-60 mx-auto w-11/12 sm:w-10/12 md:w-9/12 lg:w-8/12' id='experience'>
+            <h2 className='dark:text-white text-sky-900 font-medium text-2xl font-high-l lg:ml-[33.3%] mx-auto md:self-start lg:text-3xl dark:opacity-70'>
                 <FontAwesomeIcon icon={faBriefcase} className='mr-3' />
                 <span className='relative hor-bar'>
                     Experience
@@ -15,17 +15,17 @@ export default function Experience() {
 
             {/* <section className='flex flex-col lg:flex-row md:gap-8 lg:gap-12 justify-center items-center lg:items-start mt-8 a sm:mt-10 mx-auto w-11/12 sm:w-10/12 md:w-9/12 lg:w-11/12'> */}
 
-            <section className='font-sans font-[240] lg:w-2/3 opacity-80 mt-5 text-white text-base lg:text-[1.1rem] tracking-tight'>
+            <section className='font-sans font-[240] lg:w-2/3 dark:opacity-80 mt-5 text-base lg:text-[1.1rem] tracking-normal'>
                 {profile['experience'].map(organisation =>
                     <div>
                         <h3 className='text-xl md:text-2xl font-normal text-sky-500'><a href={organisation.url} target='_blank' rel='noreferrer'>{organisation['name']}</a></h3>
-                        <h4 className='text-base md:text-xl font-normal'>{organisation['role']}</h4>
-                        <div className="font-normal mb-3">
+                        <h4 className='text-base md:text-xl text-slate-950 dark:text-white font-normal'>{organisation['role']}</h4>
+                        <div className="font-normal mb-3 text-slate-950 dark:text-white">
                             <span>{organisation['start']}</span>
                             <span> - </span>
                             <span>{organisation['end']}</span>
                         </div>
-                        {organisation['paras'].map(para => <p className='font-high-l mt-3 font-[280] leading-6'>{para}</p>)}
+                        {organisation['paras'].map(para => <p className='font-high-l mt-3 dark:font-[280] font-[400] leading-6'>{para}</p>)}
                     </div>
                 )}
 
