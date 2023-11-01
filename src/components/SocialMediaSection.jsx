385bb5c2ac5ab6ffb6d298fce50eae7f8adc9d55
gouravrajbit <gouravr244@gmail.com>
2023-11-01 21:05:24 +0530

    Float into icons

--- src/components/SocialMediaSection.jsx
+++ src/components/SocialMediaSection.jsx
@@ -5,7 +5,7 @@ export default function SocialMediaSection({ iconSize }) {
     const profilePlatforms = ['linkedin', 'github', 'twitter', 'instagram'];
     return (
         <div className='text-white flex gap-12 align-middle'>
-            {profilePlatforms.map(platform => <SocialMediaIcon iconSize={iconSize} type={platform} key={platform} />)}
+            {profilePlatforms.map(platform => <SocialMediaIcon iconSize={iconSize} type={platform} key={platform} float={true} />)}
         </div>
     )
 }
